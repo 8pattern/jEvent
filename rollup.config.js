4fdfc825f6c54e80ce86b2d46357f729f324a607
8pattern <eighth.principle@gmail.com>
2020-06-08 00:26:30 +0800

    Use rollup instead of webpack

---
+++ rollup.config.js
@@ -0,0 +1,26 @@
+const babel = require("rollup-plugin-babel")
+const ts = require('rollup-plugin-typescript2')
+const { terser } = require('rollup-plugin-terser')
+
+const isProduction = process.env.NODE_ENV === 'production'
+
+const basePlugins = [
+  ts(),
+  babel()
+]
+const devPlugins = [
+  ...basePlugins,
+]
+const prodPlugins = [
+  ...basePlugins,
+  terser(),
+]
+
+export default {
+  input: 'src/index.ts',
+  output: {
+    file: 'dist/jEvent.js',
+    format: 'cjs',
+  },
+  plugins: isProduction ? prodPlugins : devPlugins,
+}
